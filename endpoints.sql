128c02d5e782a13d71284dcf50edae946217aaa4
Liz Vallejos <lizvalle1199@gmail.com>
2023-08-21 22:40:48 -0400

    add profile endpoints

--- endpoints.sql
+++ endpoints.sql
@@ -8,17 +8,64 @@ actions a user can be able to do
 */
 
 
---user endpoints and profile endpoits
---create user
---update user data
---delete user
---list users
+-- user endpoints and profile endpoits
+-- create user
+-- update user data
+-- delete user
+-- list users
 
 
---create a session
---create a profile
---delete a profile
---modify profile content
+-- create a session
+insert into session (user_id,ip,status, datetime) values (2,"192.168.1.6","active", now());
+-- create a profile
+insert into profile (user_id,nick_name, about, privacy, img_path, verified,is_active) values (6,"licha", "amo los gatos","publico","img_liz",1,1);
+-- delete a profile
+-- Actualizar el estado del perfil a inactivo
+UPDATE PROFILE SET is_active = false WHERE profile_id = 1;
+-- Desvincular las relaciones en otras tablas
+UPDATE ALBUM SET profile_id = NULL WHERE profile_id = 1;
+UPDATE COLLABORATORS SET profile_id = NULL WHERE profile_id = 1;
+UPDATE blocked SET profile_id_em = NULL WHERE profile_id_em = 1;
+UPDATE blocked SET profile_id_rec = NULL WHERE profile_id_rec = 1;
+UPDATE collection SET profile_id = NULL WHERE profile_id = 1;
+UPDATE COMMENT SET profile_id = NULL WHERE profile_id = 1;
+UPDATE following SET profile_id_em = NULL WHERE profile_id_em = 1;
+UPDATE following SET profile_id_rec = NULL WHERE profile_id_rec = 1;
+UPDATE notification SET profile_id_em = NULL WHERE profile_id_em = 1;
+UPDATE notification SET profile_id_rec = NULL WHERE profile_id_rec = 1;
+UPDATE rate_me SET profile_id_em = NULL WHERE profile_id_em = 1;
+UPDATE rate_me SET profile_id_rec = NULL WHERE profile_id_rec = 1;
+UPDATE reaction SET profile_id = NULL WHERE profile_id = 1;
+UPDATE saved SET profile_id = NULL WHERE profile_id = 1;
+UPDATE searches SET profile_id = NULL WHERE profile_id = 1;
+UPDATE tags SET profile_id = NULL WHERE profile_id = 1;
+UPDATE views SET profile_id = NULL WHERE profile_id = 1;
+-- considera el campo is_active, para obtener perfiles activos
+SELECT * FROM PROFILE WHERE is_active = true;
+-- consideramos datos relacionados de un perfil
+SELECT * FROM ALBUM WHERE profile_id = 1;
+SELECT * FROM COLLABORATORS  WHERE profile_id = 1;
+SELECT * FROM blocked  WHERE profile_id_em = 1;
+SELECT * FROM blocked WHERE profile_id_rec = 1;
+SELECT * FROM collection  WHERE profile_id = 1;
+SELECT * FROM COMMENT  WHERE profile_id = 1;
+SELECT * FROM following WHERE profile_id_em = 1;
+SELECT * FROM following WHERE profile_id_rec = 1;
+SELECT * FROM notification WHERE profile_id_em = 1;
+SELECT * FROM notification WHERE profile_id_rec = 1;
+SELECT * FROM rate_me WHERE profile_id_em = 1;
+SELECT * FROM rate_me WHERE profile_id_rec = 1;
+SELECT * FROM reaction WHERE profile_id = 1;
+SELECT * FROM saved  WHERE profile_id = 1;
+SELECT * FROM searches  WHERE profile_id = 1;
+SELECT * FROM tags   WHERE profile_id = 1;
+SELECT * FROM views WHERE profile_id = 1;
+
+-- modify profile content
+update profile
+set nick_name = "yurix", about= "about yuri", privacy = "private", img_path= "img_pathhyuri",verified=0,is_active=0 
+	where profile_id= 7;
+
 
 /*
 TO BE CONSIDERED
@@ -26,143 +73,143 @@ TO BE CONSIDERED
 --any search = 7 , any album = 8, any collection = 3
 */
 ----------------------------------------------------------------
---BLOCKED AND PROFILE TABLES USED
---blocking a profile
+-- BLOCKED AND PROFILE TABLES USED
+-- blocking a profile
 INSERT INTO `BLOCKED` (`profile_id_em`, `profile_id_rec`) VALUES
 (1, 2);
---unblocking a profile
+-- unblocking a profile
 DELETE FROM BLOCKED WHERE profile_id_em = 1 AND profile_id_rec = 2;
---get a list 
+-- get a list 
 SELECT * FROM BLOCKED WHERE profile_id_em = 1;
 
---FOLLOWING TABLE USED AND PROFILE TABLE
---follow a profile
+-- FOLLOWING TABLE USED AND PROFILE TABLE
+-- follow a profile
 INSERT INTO FOLLOWING (profile_id_em, profile_id_rec) VALUES (1, 2);
---unfollow a profile
+-- unfollow a profile
 DELETE FROM FOLLOWING WHERE profile_id_em = 1;
---delete follower
+-- delete follower
 DELETE FROM FOLLOWING WHERE profile_id_em = 2 AND profile_id_rec = 1;
---get a list of my followers
+-- get a list of my followers
 SELECT * FROM FOLLOWING WHERE profile_id_rec = 1;
---get a list of profiles I follow
+-- get a list of profiles I follow
 SELECT * FROM FOLLOWING WHERE profile_id_em = 1;
 
 
---RATE AND PROFILES TABLES USED
---rate a profile
+-- RATE AND PROFILES TABLES USED
+-- rate a profile
 INSERT INTO RATE_ME (profile_id_em, profile_id_rec, datetime, description, rate)
 VALUES (1, 2, NOW(), 'Great photographer!', 10.5);
---unrate a profile
+-- unrate a profile
 DELETE FROM RATE_ME WHERE profile_id_em = 1 and profile_id_rec = 2;
---get my rate
+-- get my rate
 SELECT AVG(rate) AS average_rate FROM RATE_ME WHERE profile_id_rec = 1;
---get a list of profiles I rate
+-- get a list of profiles I rate
 SELECT * FROM RATE_ME WHERE profile_id_em = 1;
 
---TAG , POST AND PROFILE TABLES USED
---create a tag post
+-- TAG , POST AND PROFILE TABLES USED
+-- create a tag post
 INSERT INTO TAGS (post_id, profile_id) VALUES (10, 2);
---untag a profile of a post
+-- untag a profile of a post
 DELETE FROM TAGS WHERE post_id = 10 AND profile_id = 2;
---tags list of a post
+-- tags list of a post
 SELECT * FROM TAGS WHERE post_id = 1;
 
 
---SEARCH TABLE USED AND PROFILE
---create a search
+-- SEARCH TABLE USED AND PROFILE
+-- create a search
 INSERT INTO SEARCHES (profile_id, search_text, datetime) VALUES (1, 'Travel destinations', NOW());
---searching records (get list)
+-- searching records (get list)
 SELECT * FROM SEARCHES WHERE profile_id = 1;
---last 6 recent records 
+-- last 6 recent records 
 SELECT * FROM SEARCHES ORDER BY datetime DESC LIMIT 6; 
---delete my records from searching table
+-- delete my records from searching table
 DELETE FROM SEARCHES WHERE search_id = 7;
 
 
---ALBUM, POST AND PROFILE TABLES USED
---create an album
+-- ALBUM, POST AND PROFILE TABLES USED
+-- create an album
 INSERT INTO ALBUM (album_name, profile_id, description) VALUES ('Vacation Album', 1, 'Our amazing trip');
---delete an album
---DELETE FROM POST WHERE album_id = 8; --its posts, but ...
+-- delete an album
+-- DELETE FROM POST WHERE album_id = 8; --its posts, but ...
 DELETE FROM ALBUM WHERE album_id = 8;
---update album
+-- update album
 UPDATE ALBUM SET description = 'Updated description' WHERE album_id = 1;
 UPDATE ALBUM SET album_name = 'not vacation but party' WHERE album_id = 1;
---get album list of a profile
+-- get album list of a profile
 SELECT * FROM ALBUM WHERE profile_id = 1;
---view content of an especific album
+-- view content of an especific album
 SELECT * FROM POST WHERE album_id = 8;
 
 
---COLLABORATOR, ALBUM AND PROFILE TABLES USED   
---create a collaboration on an album
+-- COLLABORATOR, ALBUM AND PROFILE TABLES USED   
+-- create a collaboration on an album
 INSERT INTO COLLABORATORS (profile_id, album_id) VALUES (2, 8);
---delete a album collaborator  album
+-- delete a album collaborator  album
 DELETE FROM COLLABORATORS WHERE profile_id = 2 AND album_id = 8;
---list of collaborators of an album
+-- list of collaborators of an album
 SELECT * FROM COLLABORATORS WHERE album_id = 8;
 
 
---COLLECTION AND PROFILE TABLES USED
---create a collection
+-- COLLECTION AND PROFILE TABLES USED
+-- create a collection
 INSERT INTO COLLECTION (profile_id, collection_name) VALUES (1, 'Favorites');
---get records list of a collection
+-- get records list of a collection
 SELECT * FROM COLLECTION WHERE profile_id = 1;
---update name
+-- update name
 UPDATE COLLECTION SET collection_name = 'Best of 2023' WHERE collection_id = 3;
---delete a collection
+-- delete a collection
 DELETE FROM COLLECTION WHERE collection_id = 3;
 
 
---SAVED, POST, COLLECTION AND PROFILE TABLES USED
---save a post in a collection created
+-- SAVED, POST, COLLECTION AND PROFILE TABLES USED
+-- save a post in a collection created
 INSERT INTO SAVED (profile_id, post_id, collection_id) VALUES (1, 10, 3);
---get list of post saved in a collection
+-- get list of post saved in a collection
 SELECT * FROM SAVED WHERE collection_id = 3;
---erase a post from a collection
+-- erase a post from a collection
 DELETE FROM SAVED WHERE profile_id = 1 AND post_id = 10;
 
 
---VIEWS, POST AND PROFILE TABLES USED
---create a record
+-- VIEWS, POST AND PROFILE TABLES USED
+-- create a record
 INSERT INTO VIEWS (post_id, profile_id, datetime) VALUES (10, 1, NOW());
---get records list of an post
+-- get records list of an post
 SELECT * FROM VIEWS WHERE post_id = 10;
---delete a record
+-- delete a record
 DELETE FROM VIEWS WHERE post_id = 10 AND profile_id = 1;
 
 ------------------------------------------------------------------  
 
 
---POST ENDPOINTS
---POST TABLE USED AND PROFILE TABLE
---create a post(done)
---detele a post and its tags
---...
+-- POST ENDPOINTS
+-- POST TABLE USED AND PROFILE TABLE
+-- create a post(done)
+-- detele a post and its tags
+-- ...
 
---REACTION, POST, COMMENT AND PROFILE TABLES USED
---react a post or comment
---unreact a post
---...
+-- REACTION, POST, COMMENT AND PROFILE TABLES USED
+-- react a post or comment
+-- unreact a post
+-- ...
 
---COMMENT, POST AND PROFILE TABLES USED
---create a comment on a post
---create a comment on a comment
---delete comment and its list of comments
---rewrite comment
---get list of post comments
---get list of comments of a comment
+-- COMMENT, POST AND PROFILE TABLES USED
+-- create a comment on a post
+-- create a comment on a comment
+-- delete comment and its list of comments
+-- rewrite comment
+-- get list of post comments
+-- get list of comments of a comment
 
 
---MEDIA ...
---...
+-- MEDIA ...
+-- ...
 
 --
 ------------------------------------------------------------------------------------------------
 
---NOTIFICATION, REACTION, COMMENT, RATE_ME, FOLLOWING, AND PROFILE TABLES USED
+-- NOTIFICATION, REACTION, COMMENT, RATE_ME, FOLLOWING, AND PROFILE TABLES USED
 --
 
---history
---get a list of my activity
+-- history
+-- get a list of my activity
 --
